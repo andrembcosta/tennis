54e54b3e871a4c9c446377a6cda25e1eb8ee3b2d
andrembcosta <andrembc10@gmail.com>
2026-05-02 14:15:11 -0700

    fix: use NullPool for serverless compatibility

--- api/database.py
+++ api/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.pool import NullPool
 from api.config import settings
 
-engine = create_async_engine(settings.postgres_url, echo=False)
+engine = create_async_engine(settings.postgres_url, echo=False, poolclass=NullPool)
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
